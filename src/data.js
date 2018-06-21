2ba3935f07799ee1a188cd045c417b7fd9a49779
kellyRosales <kellygrc20@gmail.com>
2018-06-21 15:00:36 -0500

    limpiando proyecto

--- src/data.js
+++ src/data.js
@@ -1,1 +1,3 @@
 //La idea de este archivo es contener toda la funcionalidad que corresponda a obtener, procesar y manipular datos.
+window.computeUsersStats = function(users, progress, courses){
+   /*extraer la función addstudents*/}
--- src/main.js
+++ src/main.js
@@ -1,29 +1,62 @@
 //En este caso puedes usar main.js para todo tu código que tenga que ver con mostrar los datos en la pantalla,
+
 const studentsButton=document.getElementById('submit-btn');
 const studentsContainer=document.getElementById('response-container');
 
-studentsButton.addEventListener('click', function(e){
+studentsButton.addEventListener('click', (e) => {
 e.preventDefault();
 getStudents();
-})
-
-function getStudents(){
+})  
+const  getStudents =()=> {
     const request = new XMLHttpRequest();
     request.open('GET','../data/cohorts/lim-2018-03-pre-core-pw/users.json');
     request.onload = addStudents;
     request.onerror = handleError;
     request.send();
-    console.log(request.responseText);
 }
-function handleError (){
+const handleError=()=>{
     console.log('error');
 }
-
-function addStudents (){
+//debe ir en data.js igual que todas las funcionalidades
+const addStudents =()=>{
     const data=JSON.parse(event.target.responseText);
+    //computeUsersStats(data)
     for (let i=0; i<data.length; i++){
         let li = document.createElement('li');
         li.innerText=data[i].name;
         studentsContainer.appendChild(li);
     }
 }
+
+//para extraer los ejercicios
+const exercisesContainer = document.getElementById('container-exercises');
+const exercisesButton =document.getElementById('btn-exercises');
+exercisesButton.addEventListener('click',(e)=> {
+e.preventDefault ();
+getExercises();
+})
+
+getExercises = ()=>{
+    const exercisesRequest = new XMLHttpRequest (); 
+    exercisesRequest.open('GET','../data/cohorts/lim-2018-03-pre-core-pw/progress.json');
+    exercisesRequest.onload=addExercises;
+    exercisesRequest.onerror= handleError;
+    exercisesRequest.send();
+};
+
+    //convertir un el objeto en un array para poder ingresar y buscar progress
+    // map es un metodo de arrays que te devuelve otro array
+addExercises=()=>{
+
+    const dataExercises = JSON.parse(event.target.response);
+    const converExercises = Object.keys(dataExercises);
+    let li = document.createElement('li');
+    li.innerHTML=dataExercises[i].
+    exercisesContainer.appendChild(li);
+    console.log(converExercises);
+
+
+    // for (let i=0; i<dataExcercises; i++){
+    //    let li=document.createElement('li');
+    //  li.innerText=dataExcercises[i].
+    }
