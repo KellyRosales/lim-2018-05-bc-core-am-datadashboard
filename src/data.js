deed648f0b0f94d1a9bd3a53e58f31ece6f6cabc
Kelly <kellygrc20@gmail.com>
2018-07-11 08:19:31 -0500

    primera función con test

--- src/data.js
+++ src/data.js
@@ -1,102 +1,94 @@
 window.computeUsersStats = (users, progress, courses) => {
-  let  
+  let mapUser = users.filter((us) => us.role === "student").map((student) => {
+      // Declaro mis variables acumuladoras
+      let percent;
+      let exercisesTotal = 0;
+      let exercisesCompleted = 0;
+      let readsTotal = 0;
+      let readsCompleted = 0;
+      let quizzesTotal = 0;
+      let quizzesCompleted = 0;
+      let scoreAvg = 0;
+      let scoreSum = 0;
 
-  const functionPercentage = user => {
-    let number = 0;
-    courses.map(course => {
-      if (user[course]) {
-        number += user[course]['percent'];
-      }
-    });
-    return number / courses.length;
-  };
-  const functionExercises = () => {
-    let totalExercises= 0;
-    let completedExercises = 0;
+      //Obteniendo porcentaje, ejercicios,reads y quizzes 
+      courses.forEach(course => {
+          if (progress[student.id].hasOwnProperty(course)) {
+              percent = progress[student.id].intro.percent;  // Obteniendo porcentajes
+              const unitStudent = progress[student.id].intro.units; //return : {01-introduction: {…}, 03-ux-design: {…}, 02-variables-and-data-types: {…}}
 
-    for (let i in resultStudents) {
-      let objCourses = progress[resultStudents[i].id]; //return: {intro: {…}}
-      if (objCourses.hasOwnProperty("intro")) {
-          let arrCourses = Object.keys(progress[resultStudents[i].id]); //return: ["intro"]
+              Object.keys(unitStudent).forEach((nameUnit) => {
+                  const partsNameUnit = unitStudent[nameUnit].parts;//return: 05-quiz: {…}, 04-guided-exercises: {…}, 01-variables: {…}, 02-self-learning-MDN: {…}, 03-comments: {…}, …}
 
-          for (let course of arrCourses) {
-              let objcourse = progress[resultStudents[i].id][course]['units']; // return: {01-introduction: {…}, 03-ux-design: {…}, 02-variables-and-data-types: {…}}
-              if (objcourse.hasOwnProperty("02-variables-and-data-types")) {
-                  let arrParts = Object.keys(progress[resultStudents[i].id][course]['units']); // return:[01-introduction: {…}, 03-ux-design: {…}, 02-variables-and-data-types: {…}]
-
-                  for (let part of arrParts) {
-                      let objPart = progress[resultStudents[i].id][course]['units'][part]['parts']; //return: {03-comments: {…}, 06-exercises: {…}, 00-values-data-types-and-operators: {…}, 05-quiz: {…}, 04-guided-exercises: {…}, …}
-                      if (objPart.hasOwnProperty("06-exercises")) {
-                          let arrSubParts = Object.keys(progress[resultStudents[i].id][course]['units'][part]['parts'])//return:["03-comments", "06-exercises", "00-values-data-types-and-operators", "05-quiz", "04-guided-exercises", "01-variables", "02-self-learning-MDN"]
-
-                          // Extracción de ejercicios
-                          for (let completed of arrSubParts) {
-                              let objCompleted = progress[resultStudents[i].id][course]['units'][part]['parts'][completed]['completed'];
-                              console.log(objCompleted)
+                  Object.keys(partsNameUnit).forEach((nameParts) => {
+                      const part = partsNameUnit[nameParts]; // return:{duration: 15, completed: 1, type: "read", completedAt: "2018-04-02T06:25:57.010Z"}
+                      const type = partsNameUnit[nameParts].type;
+                      // Ejercicios
+                      if (part.hasOwnProperty("exercises")) {
+                          const partsExercises = part.exercises; // return: {01-coin-convert: {…}, 02-restaurant-bill: {…}
+                          Object.keys(partsExercises).forEach((nameExercises) => {
+                              const recorrPartExercises = partsExercises[nameExercises];
+                              exercisesTotal += 1;
+                              exercisesCompleted += recorrPartExercises.completed;
+                          })
+                      };
+                      // Lecturas
+                      if (part.hasOwnProperty("type")) {
+                          if (type === "read") {
+                              readsTotal += 1;
+                              readsCompleted += part.completed;
                           }
-                        }
                       }
-                    }
-                  }
-                }
-              }
+                      //Quizzes
+                      if (part.hasOwnProperty("type")) {
+                          if (type === "quiz") {
+                              quizzesTotal += 1;
+                              quizzesCompleted += part.completed;
+                              scoreSum += part.score ? (part.score) : 0; // previniendo undefine
+                              scoreAvg += scoreSum / quizzesCompleted ? (scoreSum / quizzesCompleted) : 0 //Previniendo NaN, Promedio de  puntuaciones
+                          }
+                      }
+                  })
+              })
+          }
+      });
+      
+      const exercisesPercent = Math.round((exercisesCompleted/exercisesTotal)*100);
+      const readsPercent = Math.round((readsCompleted/readsTotal)*100);
+      const quizzesPercent = Math.round((quizzesCompleted/quizzesCompleted)*100);
+      
+      const usersWithStats = {
+          name: student.name.toUpperCase(),
+          stats: {
+            percent: isNaN(Math.round(percent)) ? 0 : Math.round(percent),
+            exercises: {
+              total: exercisesTotal,
+              completed: exercisesCompleted,
+              percent: exercisesPercent,
+            },
+            reads: {
+              total:readsTotal,
+              completed: readsCompleted,
+              percent: readsPercent,
+            },
+            quizzes: {
+              total: quizzesTotal,
+              completed: quizzesCompleted,
+              percent: quizzesPercent,
+              scoreSum: Math.round(scoreSum),
+              scoreAvg: Math.round(scoreAvg),
             }
-  
-  
-  const functionReads = user => {
-    let units = courses.reduce((accum, c) => user[c] && user[c].units && [...accum, ...Object.values(user[c].units)], []) || [];
-    let parts = flatArray(units.map(unit => unit.parts).map(part => Object.values(part)));
-    let partsWithReads = parts.filter(part => part.type === 'read');
-    let reads = partsWithReads
-    let completedReads = reads.filter(read => read.completed === 1).length;
-
-    return {
-      total: reads.length,
-      completed: completedReads,
-      percent: Math.round(completedReads / reads.length * 100)
-    }
-  }
-  const functionQuizess = user => {
-    let units = courses.reduce((accum, c) => user[c] && user[c].units && [...accum, ...Object.values(user[c].units)], []) || [];
-    let parts = flatArray(units.map(unit => unit.parts).map(part => Object.values(part)));
-    let partsWithQuizz = flatArray(parts.filter(part => part.type === 'quiz'));
-    let quizz = partsWithQuizz
-    let completedQuizzes = quizz.filter(quiz => quiz.completed === 1).length;
-    let sumScore1 = flatArray(parts.filter(part => part.type === 'quiz' && part.completed === 1));
-    let sumScore = sumScore1.reduce((sum, element) => {
-      return sum + element.score;
-    }, 0);
-
-    return {
-      total: quizz.length,
-      completed: completedQuizzes,
-      percent: Math.round(completedQuizzes / quizz.length * 100),
-      scoreSum: sumScore,
-      scoreAvg: Math.round(sumScore / completedQuizzes)
-    }
-  }
-
-  let students = users.filter((user) => user.role === 'student').map(user => {
-    const progressUser = progress[user.id];
-    return ({
-      id: user.id,
-      stats: {
-        name: user.name,
-        percent: functionPercentage(progressUser),
-        exercises: functionExercises(progressUser),
-        reads: functionReads(progressUser),
-        quizzes: functionQuizess(progressUser),
-      }
-    });
-    // return user;
-  });
-  return students;
+          }
+        }
+        return usersWithStats
+  })
+  return mapUser;
 }
 
-// window.sortUsers = (users, orderBy, orderDirection) => { }
-// window.filterUsers = (users, search) => { }
 window.processCohortData = (options) => {
   const courses = Object.keys(options.cohort.coursesIndex);
   let users = computeUsersStats(options.cohortData.users, options.cohortData.progress, courses);
   return users
 }
+
+
--- src/main.js
+++ src/main.js
@@ -48,25 +48,26 @@ const callbackCohorts = (sede, dataCohorts) => {
 
 const callbackProgress = (studentProgress, dataProgress) => {
     options.cohortData.progress = dataProgress;
-    const arrayFourFunction = processCohortData(options);
+    const fourFunction = processCohortData(options);
+   
     studentsContainer.innerHTML= '';
-    for (const users of arrayFourFunction ){
-        const table = document.createElement('tr')
-        table.innerHTML +=`
-            <td >${users.stats.name}</td>
-            <td >${users.stats.percent}</td>
-            <td >${users.stats.exercises.completed}</td>
-            <td >${users.stats.reads.completed}</td>
-            <td>${users.stats.quizzes.completed}</td>
+    for (const users of fourFunction ){
+    const row = document.createElement('tr')
+       row.innerHTML +=`
+              <th scope="row"> ${users.namef} </th>
+              <td >${users.stats.percent}</td>
+              <td >${users.stats.exercises.completed}</td>
+              <td >${users.stats.reads.completed}</td>
+              <td>${users.stats.quizzes.completed}</td>       
         `
-        studentsContainer.appendChild(table)
+        studentsContainer.appendChild(row)
     }
 }
 
 const callbackUsers = (person, dataUsers) => {
     options.cohortData.users = dataUsers;
     //Extrayendo los archivos JSON de progress
-    getData(person, `../../data/cohorts/${person}/progressPrueba.json`, callbackProgress);
+    getData(person, `../../data/cohorts/${person}/progress.json`, callbackProgress);
 }
 
 // Eventos click
@@ -77,12 +78,12 @@ listVenue.addEventListener('click', (event) => {
 //Extrayendo los archivos JSON de users
 generationContainer.addEventListener('click', (event) => {
     //reasignando el objeto options
-    options.cohort.forEach((generation) => {
-        if (generation.id === event.target.id) {
-            options.cohort = generation;
+    options.cohort.forEach((element) => {
+        if (element.id === event.target.id) {
+            options.cohort = element;
         }
     });
 
-    getData(event.target.id, `../../data/cohorts/${event.target.id}/usersPrueba.json`, callbackUsers)
+    getData(event.target.id, `../../data/cohorts/${event.target.id}/users.json`, callbackUsers)
 });
 
